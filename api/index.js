0f3b22d483ac959c011f804c155cc523daa2f6f6
conbec <56614504+conbec@users.noreply.github.com>
2020-02-24 09:59:03 -0800

    api index done, moved some files around

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
 const express = require('express');
 
-const servicesRouter = require('./services')
+const { servicesRouter } = require('./services');
+const { trucksRouter } = require('./trucks');
+const { scheduleRouter } = require('./schedule');
+const apiRouter = express.Router();
+
+apiRouter.use('/services', servicesRouter);
+apiRouter.use('/trucks', trucksRouter);
+apiRouter.use('/schedule', scheduleRouter);
+
+module.exports = {
+  apiRouter
+};
+
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { apiRouter } = require('./api/index');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/', express.static(path.join(__dirname, 'assets'))); // this might work to keep index.html in the assets directory
 app.use('/api', apiRouter);
 
 app.listen(PORT, () => console.log('Listening on port ', PORT));
